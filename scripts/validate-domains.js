709119f208babad4df0b731e827e331581ede2de
alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-05-03 20:50:00 +0200

    Added new REPO CONST validate-domains.js

--- scripts/validate-domains.js
+++ scripts/validate-domains.js
@@ -1,6 +1,8 @@
 import { execSync } from "child_process";
+
 const PR_NUMBER = process.env.PR_NUMBER;
 const GH_TOKEN = process.env.GH_TOKEN;
+const REPO = "alexmarpar/safeinternet";
 
 const diff = execSync("git diff origin/main...HEAD").toString();
 
@@ -9,45 +11,47 @@ const domainRegex = /^[a-z0-9.-]+\.[a-z]{2,}$/;
 const lines = diff.split("\n");
 
 let hasError = false;
+let invalidDomains = [];
 
 for (const line of lines) {
   if (line.startsWith("+") && !line.startsWith("+++")) {
-    const domain = line.slice(1).trim();
+    const domain = line.slice(1).trim().toLowerCase();
 
     if (!domainRegex.test(domain)) {
-      console.error("❌ Invalid domain:", domain);
       hasError = true;
-      await fetch(
-        `https://api.github.com/repos/alexmarpar/safeinternet/issues/${PR_NUMBER}/comments`,
-        {
-          method: "POST",
-          headers: {
-            Authorization: `token ${GH_TOKEN}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            body: "Error, invalid domain:",domain,
-          }),
-        },
-      );
+      invalidDomains.push(domain);
     }
   }
 }
 
 if (hasError) {
+  await fetch(
+    `https://api.github.com/repos/${REPO}/issues/${PR_NUMBER}/comments`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `token ${GH_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        body: `Invalid domains:\n${invalidDomains.join("\n")}`,
+      }),
+    }
+  );
+
   process.exit(1);
 }
 
 await fetch(
-  `https://api.github.com/repos/alexmarpar/safeinternet/issues/${PR_NUMBER}/comments`,
+  `https://api.github.com/repos/${REPO}/issues/${PR_NUMBER}/comments`,
   {
     method: "POST",
     headers: {
-      Authorization: `token ${GITHUB_TOKEN}`,
+      Authorization: `token ${GH_TOKEN}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      body: "✅ All new domains are valid",
+      body: " All new domains are valid",
     }),
-  },
+  }
 );
